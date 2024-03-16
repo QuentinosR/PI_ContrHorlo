d8f9704577981e2ed579b57027bd195b5d84aafa
Rod_lenovo_linux <quentin.rod@master.hes-so.ch>
2024-03-16 20:26:43 +0100

    10Hz ok

--- main.cpp
+++ main.cpp
@@ -12,19 +12,19 @@
 #define MIN_DELAY_MS 100  // Minimum delay between interrupts
 #define MAX_DELAY_MS 500  // Maximum delay between interrupts
 
-int current_delay_ms = 250;  // Initial delay
 
-bool led_callback(struct repeating_timer *t)
+volatile int tTrigMs = 100;
+volatile int tTcMS = 1;
+volatile bool trigOutputState = false;
+
+bool timer_trig_callback(struct repeating_timer *t)
 {
-    static bool value = true;
-    current_delay_ms = value ? 250 : 100;
-
-    gpio_put(LED_PIN, value);
-    value = !value;
-
+    int stateDuration = trigOutputState ? tTcMS : tTrigMs - tTcMS;
     // Reset the timer with the updated delay
     cancel_repeating_timer(t);
-    add_repeating_timer_ms(current_delay_ms, led_callback, NULL, t);
+    add_repeating_timer_ms(stateDuration, timer_trig_callback, NULL, t);
+    gpio_put(LED_PIN, trigOutputState);
+    trigOutputState = !trigOutputState;
 
     return true;
 }
@@ -40,7 +40,7 @@ int main()
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    if(!add_repeating_timer_ms(current_delay_ms, led_callback, NULL, &timer)) {
+    if(!add_repeating_timer_ms(1000, timer_trig_callback, NULL, &timer)) { //arbitrary time
         printf("Error: Failed to setup the timer");
         return 1;
     }
