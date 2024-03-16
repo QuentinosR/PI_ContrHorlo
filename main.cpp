f5e9cda4cc858285200693b9822bef455e4d30e0
Rod_lenovo_linux <quentin.rod@master.hes-so.ch>
2024-03-16 20:43:02 +0100

    add schematic

--- main.cpp
+++ main.cpp
@@ -16,23 +16,16 @@
 volatile int tTrigMs = 100;
 volatile int tTcMS = 1;
 volatile bool trigOutputState = false;
+volatile bool timTrigElapsedFlag = false;
+struct repeating_timer timer;
 
 bool timer_trig_callback(struct repeating_timer *t)
 {
-    int stateDuration = trigOutputState ? tTcMS : tTrigMs - tTcMS;
-    // Reset the timer with the updated delay
-    cancel_repeating_timer(t);
-    add_repeating_timer_ms(stateDuration, timer_trig_callback, NULL, t);
-    gpio_put(LED_PIN, trigOutputState);
-    trigOutputState = !trigOutputState;
-
+    timTrigElapsedFlag = true;
     return true;
 }
 
-int main()
-{
-    struct repeating_timer timer;
-
+init init(){
     stdio_init_all();
 
     printf("Hello from blinky example\n");
@@ -45,8 +38,31 @@ int main()
         return 1;
     }
 
+}
+
+//Two states : OFF and ON
+// <-tTcMS->
+//<------------tTrigMS-------->
+//__________                   __________
+//|        |___________________|        |__...
+void trig_SM_process(){
+    if(trig_SM_process){
+        timTrigElapsedFlag = false; //reset flag
+        int stateDuration = trigOutputState ? tTcMS : tTrigMs - tTcMS;
+        // Reset the timer with the updated delay
+        cancel_repeating_timer(&timer);
+        add_repeating_timer_ms(stateDuration, timer_trig_callback, NULL, &timer);
+        gpio_put(LED_PIN, trigOutputState);
+        trigOutputState = !trigOutputState;
+    }
+}
+
+int main()
+{
     while(1)
         tight_loop_contents();
+        trig_SM_process();
+
 
     return 0;
 }
