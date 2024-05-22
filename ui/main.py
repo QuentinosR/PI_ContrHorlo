e83dc3f49e0b40b62ab06b004cbc2b4ef65eccee
Rod_lenovo_linux <quentin.rod@master.hes-so.ch>
2024-05-22 21:43:52 +0200

    add shift

--- ui/main.py
+++ ui/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QSlider, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QSlider, QLabel, QLineEdit
 from PyQt6.QtGui import QPalette, QColor
 from PyQt6.QtCore import Qt
 import serial
@@ -14,19 +14,60 @@ class Flasher():
 
         self.ser.write(";".encode())
 
-        s = self.ser.read(120)
-        return s.decode()
+        #s = self.ser.read(120)
+        #return s.decode()
     def on(self):
         self._send_cmd("trig.en:1;")
     def off(self):
-        self._send_cmd("trig.en:0;")
+        self._send_cmd("trig.en:0;") 
+    def trig_off(self, val):
+        self._send_cmd("trig.off:" + str(val) +";")
+    def trig_shift(self, val):
+        self._send_cmd("trig.shift:" + str(val) +";")
+    def flash_on(self, val):
+        self._send_cmd("flash.on:" + str(val) +";")
+    def flash_off(self, val):
+        self._send_cmd("flash.off:" + str(val) +";")
 
-flasher = Flasher('/dev/ttyACM0', 115200)
 
-class slider(QWidget):
+flasher = Flasher('/dev/ttyACM1', 115200)
+
+class OneTimeEvent(QWidget):
+    def __init__(self, text, cb_parent_click):
+        super(OneTimeEvent, self).__init__()
+        self.text_val = text
+        self.parent_cb = cb_parent_click
+
+        self.setMaximumHeight(100)
+    
+        self.text = QLabel("", self)
+        self.text.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.field = QLineEdit()
+        self.field.setFixedWidth(75)
+
+        self.button = QPushButton('Send one time')
+        self.button.clicked.connect(self.cb_button) 
+        self.update_text("0")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.text)
+        layout.addWidget(self.field, alignment= Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+    def update_text(self, new_val):
+        self.text.setText(self.text_val + ": " + str(new_val))
+
+    def cb_button(self):
+        val = self.field.text()
+        self.update_text(val)
+        self.parent_cb(val)
+
+class Slider(QWidget):
 
     def __init__(self, text, cb_parent_val_changed, min, max, default):
-        super(slider, self).__init__()
+        super(Slider, self).__init__()
         self.text_val = text
         self.parent_cb = cb_parent_val_changed
 
@@ -35,17 +76,22 @@ class slider(QWidget):
         self.sl = QSlider(Qt.Orientation.Horizontal, self)
         self.sl.setRange(min, max)
         self.sl.setValue(default)
-        self.sl.setSingleStep(5)
+        self.sl.setSingleStep(150)
         self.sl.valueChanged.connect(self.cb)
 
         self.text = QLabel("", self)
         self.text.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.update_text(default) 
 
+        self.field = QLineEdit()
+        self.field.setFixedWidth(75)
+        self.field.textChanged.connect(self.cb)
+
         layout = QVBoxLayout()
         layout.addWidget(self.text)
         layout.addWidget(self.sl)
-        self.setLayout(layout)     
+        layout.addWidget(self.field, alignment= Qt.AlignmentFlag.AlignCenter)
+        self.setLayout(layout)
 
     def update_text(self, new_val):
         self.text.setText(self.text_val + ": " + str(new_val))
@@ -53,7 +99,6 @@ class slider(QWidget):
     def cb(self, val):
         self.update_text(val)
         self.parent_cb(val)
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -68,9 +113,16 @@ class MainWindow(QMainWindow):
         self.on_button.setCheckable(True)
         self.on_button.clicked.connect(self.on_button_clicked)
 
-        self.trig_off_sl = slider('Off time (us)', self.trig_off_sl_changed, 1000, 1000000, 100000)
+        self.trig_off_sl = Slider('Trig off time (us)', self.trig_off_sl_changed, 1000, 1000000, 100000)
+        self.flash_off_sl = Slider('Flash off time (us)', self.flash_off_sl_changed, 1, 100000, 10000)
+        self.flash_on_sl = Slider('Flash on time (us)', self.flash_on_sl_changed, 1, 100000, 10000)
+        self.trig_shift_ote = OneTimeEvent('Trig shift time (us)', self.trig_shift_changed)
+
         layout.addWidget(self.on_button)
         layout.addWidget(self.trig_off_sl)
+        layout.addWidget(self.flash_off_sl)
+        layout.addWidget(self.flash_on_sl)
+        layout.addWidget(self.trig_shift_ote)
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -84,6 +136,16 @@ class MainWindow(QMainWindow):
             flasher.off()
             self.on_button.setText("On")
     def trig_off_sl_changed(self, value):
+        flasher.trig_off(value)
+        print(value)
+    def flash_off_sl_changed(self, value):
+        flasher.flash_off(value)
+        print(value)
+    def flash_on_sl_changed(self, value):
+        flasher.flash_on(value)
+        print(value)
+    def trig_shift_changed(self, value):
+        flasher.trig_shift(value)
         print(value)
         
 
